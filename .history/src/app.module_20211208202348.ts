c4cb88ed32f2e8a044fc67c92174ea170dd22892
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2021-12-08 20:40:40 +0200

    delete history file

--- .history/src/app.module_20211208202348.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
-@Module({
-  imports: [],
-
-})
-export class AppModule {}
